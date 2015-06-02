cfc7896a5dc0d5324920451820c01c2a059390d5
Robert Johnston <rfjohnstonunicef@gmail.com>
2015-06-02 16:43:20 +0200

    scrollable

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,37 +1,23 @@
-require jquery
-require bootstrap-sprockets
-require_tree
+//require jquery
+//require bootstrap-sprockets
+//require_tree
+
+// Set div height to exact window height on page load
 
 
-<script type="text/javascript">
-  var i=0;
-  var k=0;
-  var dv;
-  var speed=60;     /* adjust this value to suit */
-function init() {
-   dv=document.getElementsByTagName('div');
-for(c=0;c<dv.length;c++) {
-if(dv[c].className=='clouds') {
-   dv[c].number=k++;
-  }
- }
-   rollClouds();
- }
-function rollClouds() {
-for(var c=0;c<dv.length;c++) {
-if(dv[c].className=='clouds') {
-if(dv[c].number%2==0) {
-   dv[c].style.backgroundPosition=i+'px';
- }
-else {
-   dv[c].style.backgroundPosition=-i+'px';
-   }
-  }
- }
-   i++;  /* to change direction use  i--;  */
-   scroller=setTimeout(function(){rollClouds()},speed);
- }
-   window.addEventListener?
-   window.addEventListener('load',init,false):
-   window.attachEvent('onload',init);
-</script>
+// Smooth Scrolling
+$(function() {
+   $(".window-height").css("height", $(window).height());
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
